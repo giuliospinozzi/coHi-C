0e72fea3e19612c6ff4353ac392646e9629cbf4e
Giulio Spinozzi <giuliospinozzi.phd@gmail.com>
2021-07-09 12:39:43 +0200

    [FIX] some fixes

--- pipelines/hic_pipeline.postJuicer.HiCexplorer.sh
+++ pipelines/hic_pipeline.postJuicer.HiCexplorer.sh
@@ -56,7 +56,7 @@ conda activate hic
 
 for k in $(ls -d $WORKING_DIR/*); do
     printf "\n###############################################"
-    printf "Processins Sample --> ${k#"$WORKING_DIR/"}"
+    printf "\n ----- Processins Sample --> ${k#"$WORKING_DIR/"}"
     printf "\n############################################### \n"
     SAMPLE=${k#"$WORKING_DIR/"}
     cd $WORKING_DIR/$SAMPLE
@@ -64,9 +64,6 @@ for k in $(ls -d $WORKING_DIR/*); do
     printf "\n>>>>>>>>>> ${SAMPLE} --> hicConvertFormat \n"
     hicConvertFormat -m inter_30.hic --inputFormat hic --outputFormat cool -o inter_30.cool --resolutions 10000
     hicConvertFormat -m inter_30_10000.cool --inputFormat cool --outputFormat h5 -o inter_30_10000.h5
-    # Edit for specific project
-    hicConvertFormat -m inter_30.hic --inputFormat hic --outputFormat cool -o inter_30_chr2.cool --resolutions 10000 --chromosome 2
-    hicConvertFormat -m inter_30.hic --inputFormat hic --outputFormat cool -o inter_30_chr11.cool --resolutions 10000 --chromosome 11
 
     printf "\n>>>>>>>>>> ${SAMPLE} --> hicCorrectMatrix \n"
     hicCorrectMatrix correct -m inter_30_10000.h5 --filterThreshold -1.5 5 -o inter_30_10000.corrected.h5
@@ -74,16 +71,21 @@ for k in $(ls -d $WORKING_DIR/*); do
     printf "\n>>>>>>>>>> ${SAMPLE} --> hicMergeMatrixBins \n"
     hicMergeMatrixBins -m inter_30_10000.corrected.h5 -o inter_30_10000.corrected.nb50.h5 -nb 50
 
-    printf "\n>>>>>>>>>> ${SAMPLE} --> hicPCA \n"
-    hicPCA --matrix inter_30_10000.corrected.h5 -o pca1.bedgraph pca2.bedgraph --chromosomes 2 11 -f bedgraph
+    printf "\n>>>>>>>>>> ${SAMPLE} --> hicPCA \n" ##### Too slow!!!!
+    #hicPCA --matrix inter_30_10000.corrected.h5 -o pca1.bedgraph pca2.bedgraph -f bedgraph
 
     printf "\n>>>>>>>>>> ${SAMPLE} --> hicPlotMatrix \n"
     # Edit for specific project
-    hicPlotMatrix -m inter_30_10000.corrected.h5 --clearMaskedBins --region chr2:60221755-61279177 -o ${SAMPLE}_10000_log2_chr2_60221755-61279177_matrix_plot.png
-    hicPlotMatrix -m inter_30_10000.corrected.h5 --clearMaskedBins --region chr11:4769502-5825416 -o ${SAMPLE}_10000_log2_chr11_4769502-5825416_matrix_plot.png
+    hicPlotMatrix -m inter_30_10000.corrected.h5 --clearMaskedBins --region chr2:60221755-61279177 -o ${SAMPLE}_10000_log2_chr2_60221755-61279177_matrix_plot.png --log1p
+    hicPlotMatrix -m inter_30_10000.corrected.h5 --clearMaskedBins --region chr11:4769502-5825416 -o ${SAMPLE}_10000_log2_chr11_4769502-5825416_matrix_plot.png --log1p
 
     printf "\n>>>>>>>>>> ${SAMPLE} --> hicFindTADs \n"
-    hicFindTADs -m inter_30_10000.corrected.h5 --outPrefix tads_hic_corrected --numberOfProcessors $MAXTHREADS --chromosomes 2 11 --correctForMultipleTesting fdr --maxDepth 100000 --thresholdComparisons 0.05 --delta 0.01
+    hicFindTADs -m inter_30_10000.corrected.h5 --outPrefix tads_hic_corrected --numberOfProcessors $MAXTHREADS --correctForMultipleTesting fdr --maxDepth 100000 --thresholdComparisons 0.05 --delta 0.01
+    
+    printf "\n>>>>>>>>>> ${SAMPLE} --> hicPlotTADs \n"
+    # Edit for specific project
+    hicPlotTADs --tracks track.ini -o ${SAMPLE}_TADs_chr11_5019502-5575416_track.png --region chr11:5019502-5575416
+    hicPlotTADs --tracks track.ini -o ${SAMPLE}_TADs_chr2_60471755-61029177_track.png --region chr2:60471755-61029177
 done
 
 
@@ -99,5 +101,4 @@ cd $WORKING_DIR/
 #hicPlotTADs --tracks track.ini -o ${SAMPLE}_TADs_chr11_5019502-5575416_track.png --region chr11:5019502-5575416
 #hicPlotTADs --tracks track.ini -o ${SAMPLE}_TADs_chr2_60471755-61029177_track.png --region chr2:60471755-61029177
 
-#hicDifferentialTAD -tm UT1.100000_chr2.cool -cm E22.100000_chr2.cool -td UT1_domains.bed -o differential -p 0.01 -t 4 -mr all
-#hicDifferentialTAD -tm UT1.100000_chr11.cool -cm E22.100000_chr11.cool -td UT1_domains.bed -o differential -p 0.01 -t 4 -mr all
+#hicDifferentialTAD -tm UT1/inter_30_10000.corrected.h5 -cm E22/inter_30_10000.corrected.h5 -td UT1/UT1_domains.bed -o anayslis/differential_tads -p 0.01 -t 4 -mr all
