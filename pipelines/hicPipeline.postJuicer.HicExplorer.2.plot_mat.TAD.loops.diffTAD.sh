26bfaa3b0a44e2240093de7867268a2a0703637f
alessiosportelli <sportellialessio7@gmail.com>
2021-11-22 23:46:16 +0100

    [FIX] changes according to path_dir modification
    in this version, tracks.ini is created outside RESOLUTION:resolution directory.
    path_dir in file ($1) changes to make easier differentialTAD command

--- pipelines/hicPipeline.postJuicer.HicExplorer.2.plot_mat.TAD.loops.diffTAD.sh
+++ pipelines/hicPipeline.postJuicer.HicExplorer.2.plot_mat.TAD.loops.diffTAD.sh
@@ -8,17 +8,19 @@ MAXTHREADS=$3
 
 treated=()
 untreated=()
+project_path=()
 
 while IFS=$'\t' read -r sample ID path_dir matrix_name T_UT plotMatrix_coor1 plotMatrix_coor2 plotTADs_coor1 plotTADs_coor2 CorrectMatrix_Chr  #salvo ogni valore di ogni colonna in una variabile mentre leggo tutte le ri>
 do
   arr=(${sample} ${ID} ${path_dir} ${matrix_name} ${T_UT} ${plotMatrix_coor1} ${plotMatrix_coor2} ${plotTADs_coor1} ${plotTADs_coor2} ${CorrectMatrix_Chr}) #salvo le variabili in un array per comodità
   echo "Processing sample ${arr[0]}"
   cd ${arr[2]}
-  mkdir ${RESOLUTION}_resolution/analysis  #creo una cartella in cui andranno i file rejected, accepted prodotti da hicDifferentialTADs
-  echo "la working directory è: ${arr[2]}"
+  mkdir analysis  #creo una cartella in cui andranno i file rejected, accepted prodotti da hicDifferentialTADs
+  cd ${arr[2]}/${arr[0]}
+  echo "la working directory è: ${arr[2]}/${arr[0]}"
 
    ##ciclo tra le 2 diverse regioni da plottare per lo stesso plot (colonna 5 e 6) e salvo la coordinata per intero in "coor", poi in due while separo "chr" da "start" ed "end" per poterli inserire nel nome del file output. sfrutto i cicli per scrivere una sola volta il comando hicPlotMatrix
-   printf "\n>>>>>>>>>> ${arr[0]}} --> hicPlotMatrix \n" 
+   printf "\n>>>>>>>>>> ${arr[0]} --> hicPlotMatrix \n" 
    for coor_matrix in ${arr[@]:5:2};
    do
      while IFS=":" read -r chr start_end; do
@@ -38,7 +40,7 @@ do
       while IFS=":" read -r chr start_end; do
        while IFS="-" read -r start end; do 
 
-       hicPlotTADs --tracks ${RESOLUTION}_resolution/tracks.ini -o ${RESOLUTION}_resolution/${arr[3]}_TADs_${chr}_${start}-${end}_track.png --region ${coor_TADs} --dpi 300
+       hicPlotTADs --tracks tracks.ini -o ${RESOLUTION}_resolution/${arr[3]}_TADs_${chr}_${start}-${end}_track.png --region ${coor_TADs} --dpi 300
        hicPlotMatrix -m ${RESOLUTION}_resolution/${arr[3]}_${RESOLUTION}.corrected.h5 -o ${RESOLUTION}_resolution/${arr[0]}_${RESOLUTION}_${chr}_${start}-${end}_matrix_loop.png --log1p --region ${coor_TADs} --loops ${RESOLUTION}_resolution/loops.bedgraph --dpi 300
 
          done <<< ${start_end}
@@ -48,12 +50,19 @@ do
 #salviamo i path dei campioni T nell'array "treated" e quelli dei campioni UT nell array "untreated" per l analisi differenziale successiva
 
    if [ ${arr[4]} = "T" ]; then
-      echo "aggiungo il campione all array treated"
-      treated+=(${arr[2]})
+      echo "aggiungo il nome del campione all array treated"
+      treated+=(${arr[0]})
    else
-      echo "aggiungo il campione all array untreated"
-      untreated+=(${arr[2]})
+      echo "aggiungo il nome del campione all array untreated"
+      untreated+=(${arr[0]})
    fi
+
+#salvo il path del progetto nella variabile "project_path" inizializzata all'esterno del while all inizio
+   if [ "${#project_path[@]}" -lt 1 ]; then
+    project_path+=(${arr[2]})
+   fi
+   echo ${project_path[@]}
+
 
 done < <(tail -n +2 ${FILE})  #fornisco il file da leggere e dico che voglio leggere dalla linea 2 (skippo l'header)
 
@@ -66,12 +75,10 @@ echo " i campioni untreated sono: ${untreated[@]}"
 #però se faccio a parte ho il problema del nome del sample e del nome della matrice (inter_30)
 
   printf "\n >>>>>>>>>> hicDifferentialTAD \n"
-  for path_t in $treated; do
-    t_sample=$(grep -o '[^/]*$' <<< ${path_t})  #salvo il nome del sample nella variabile t_sample prendendolo dal path
-      for path_ut in $untreated; do
-       ut_sample=$(grep -o '[^/]*$' <<< ${path_ut}) #salvo il nome del sample nella variabile ut_sample prendendolo dal path
-      #capire in che cartella salvare questi file
-      hicDifferentialTAD -cm ${path_ut}/${RESOLUTION}_resolution/inter_30_${RESOLUTION}.corrected.h5 -tm ${path_t}/${RESOLUTION}_resolution/inter_30_${RESOLUTION}.corrected.h5 -td ${path_t}/${RESOLUTION}_resolution/tads_hic_corrected_domains.bed -o  ${RESOLUTION}_resolution/analysis/differential_tads_${ut_sample}-${t_sample} -p 0.01 -t 4 -mr all --threads ${MAXTHREADS}
+  for t_sample in $treated; do
+     for ut_sample in $untreated; do
+
+      hicDifferentialTAD -cm ${project_path[0]}/${ut_sample}/${RESOLUTION}_resolution/inter_30_${RESOLUTION}.corrected.h5 -tm ${project_path[0]}/${t_sample}/${RESOLUTION}_resolution/inter_30_${RESOLUTION}.corrected.h5 -td ${project_path[0]}/${t_sample}/${RESOLUTION}_resolution/tads_hic_corrected_domains.bed -o ${project_path[0]}/analysis/differential_tads_${ut_sample}-${t_sample} -p 0.01 -t 4 -mr all --threads ${MAXTHREADS}
 
       done
   done
