29feef0cce0b03441bb41683d5a86d6457c13808
alessiosportelli <sportellialessio7@gmail.com>
2021-11-22 23:40:13 +0100

    [FIX] changes according to pat_dir modification + mcool conversion
    path_dir in file ($1) changes to make easier the differentialTADs command in script2.
    .mcool conversion is added for hicrep subsequent step

--- pipelines/hicPipeline.postJuicer.HicExplorer.1.matConv.findTADs.sh
+++ pipelines/hicPipeline.postJuicer.HicExplorer.1.matConv.findTADs.sh
@@ -11,15 +11,16 @@ do
   arr=($sample $ID $path_dir $matrix_name $T_UT $plotMatrix_coor $plotTADs_coor $CorrectMatrix_Chr) #salvo le variabili in un array per comodità
   echo "Processing sample ${arr[0]}"
 
-  echo ${arr[2]} #3° colonna: path (indici partono da 0)
-  cd ${arr[2]}
-  echo "la working directory è: ${arr[2]}"
+  echo ${arr[2]}/${arr[0]} #3° colonna: path project directory + sample name (indici partono da 0)
+  cd ${arr[2]}/${arr[0]}
+  echo "la working directory è: ${PWD}"
   mkdir ${RESOLUTION}_resolution
 
 
   printf "\n >>>>>>>>> ${arr[0]} --> hicConverFormat \n"
   hicConvertFormat -m ${arr[3]}.hic  --inputFormat hic --outputFormat cool -o ${RESOLUTION}_resolution/${arr[3]}.cool --resolution ${RESOLUTION}
   hicConvertFormat -m ${RESOLUTION}_resolution/${arr[3]}_${RESOLUTION}.cool --inputFormat cool --outputFormat h5 -o ${RESOLUTION}_resolution/${arr[3]}_${RESOLUTION}.h5
+  hicConvertFormat -m ${RESOLUTION}_resolution/${arr[3]}_${RESOLUTION}.cool --inputFormat cool --outputFormat mcool -o ${RESOLUTION}_resolution/${arr[3]}.mcool --resolutions ${RESOLUTION}
 
   printf "\n >>>>>>>>> ${arr[0]} --> hicNormalize \n"
   hicNormalize -m ${RESOLUTION}_resolution/${arr[3]}_${RESOLUTION}.h5 -n smallest -o ${RESOLUTION}_resolution/${arr[3]}_${RESOLUTION}.normalized.h5
@@ -33,10 +34,8 @@ do
   printf "\n >>>>>>>>> ${arr[0]} --> hicDetectLoops \n"
   hicDetectLoops -m ${RESOLUTION}_resolution/${arr[3]}_${RESOLUTION}.cool -o ${RESOLUTION}_resolution/loops.bedgraph --maxLoopDistance 2000000 --windowSize 10 --peakWidth 6 --pValuePreselection 0.05 --pValue 0.05 --threads ${MAXTHREADS}
 
-  printf "\n >>>>>>>>>> ${arr[0]} --> make_tracks_file \n"
-  make_tracks_file --trackFiles ${RESOLUTION}_resolution/${arr[3]}_${RESOLUTION}.corrected.h5 ${RESOLUTION}_resolution/tads_hic_corrected_boundaries.bed -o tracks.ini
-
-
+ printf "\n >>>>>>>>>> ${arr[0]} --> make_tracks_file \n"
+ make_tracks_file --trackFiles ${RESOLUTION}_resolution/${arr[3]}_${RESOLUTION}.corrected.h5 ${RESOLUTION}_resolution/tads_hic_corrected_boundaries.bed -o tracks.ini 
 
 
 done < <(tail -n +2 ${FILE}) #fornisco il file da leggere e dico che voglio leggere dalla linea 2 (skippo l'header)
