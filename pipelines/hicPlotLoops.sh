3966867732c1bf42cdba6dc2259224bf4ac76c59
alessiosportelli <75388781+alessiosportelli@users.noreply.github.com>
2022-07-26 15:30:02 +0200

    Updated hicPlotLoops.sh and hicPlotTADs.sh version
    Updated version with respect to the last pipe modifications

--- pipelines/hicPlotLoops.sh
+++ pipelines/hicPlotLoops.sh
@@ -2,11 +2,11 @@
 
 ### HIC PLOT TADS ### - for multiple resolution, for multiple regions, for multiple samples.
 
-#Script in cui plotto soltanto i TADs. 
-#Input necessari: tracks.ini (e quindi la matrice .corrected.h5 e tads_hic_corrected_domains.bed) - lo trova in automatico
+#Script in cui plotto soltanto i Loops. 
+#Input necessari: inter_30_${res_arr[i]}_corrected.h5, loops.bedgraph.  li trova in automatico perchè gli passo il path assoluto
 
 #EXECUTION EXAMPLE:
-#./hicPlotTADs.sh /home/alessio/hic/complete_hic_pipe_exe/momentaneo_ADA_da_eliminare/association_file_shallow_ADA.tsv 5000,10000 8 chr11:33875000-34135000,chr11:33864303-34012900
+#./hicPlotLoops.sh /home/alessio/hic/complete_hic_pipe_exe/momentaneo_ADA_da_eliminare/association_file_shallow_ADA.tsv 5000,10000 8 chr11:33875000-34135000,chr11:33864303-34012900
 
 
 FILE=$1	#association file
@@ -17,10 +17,11 @@ COORDINATES=$4  #coordinate delle regioni da plottare (comma separated)
 
 arr_coor=()
 IFS=',' read -r -a arr_coor <<< "${COORDINATES}" 
-echo "l'array contenente le coordinate è:  ${arr_coor[@]}" 
+echo "l'array contenente le coordinate è:  ${arr_coor[@]}\n" 
 
 
-printf "\n>>>>>>>>>> ${arr[0]} --> hicPlotTADs \n" #plotto ogni regione, per ogni campione, una volta per ogni risoluzione desiderata. Ad es, risoluzione 5000, 10000 ecc.
+
+printf "\n>>>>>>>>>> ${arr[0]} --> hicPlotMatrix with loops \n" #plotto ogni regione, per ogni campione, una volta per ogni risoluzione desiderata. Ad es, risoluzione 5000, 10000 ecc.
 
 res_arr=() 
 IFS=',' read -r -a res_arr <<< "${RESOLUTION}" 
@@ -31,7 +32,7 @@ arr_len="${#res_arr[@]}"  #lunghezza dell array 1 (che è uguale a quella dell'a
 #ciclo per ogni risoluzione
 for (( i=0; i<=${arr_len}-1; i++ ))    #faccio -1 perchè arr_len è il numero di elementi dell'array, ma gli indici iniziano da 0, quindi devo sottrarre 1 al num totale di elementi 
 do
-echo -e "--- Executing hicPlotTADs with resolution ${res_arr[i]}"
+echo -e "--- Executing hicPlotMatrix with loops with resolution ${res_arr[i]}\n"
 	#ciclo per ogni coordinata
 	for coor in ${arr_coor[@]}
 	do
@@ -45,17 +46,16 @@ echo -e "--- Executing hicPlotTADs with resolution ${res_arr[i]}"
 	  	cd ${arr[2]}
 	  	
 	  	#Creating "fastq" dir in "topDir" if its not already present 
-		if [ ! -d "plotTADs_${res_arr[i]}_${coor}" ]; then
-    		mkdir plotTADs_${res_arr[i]}_${coor}  
+		if [ ! -d "plotLoops_${res_arr[i]}_${coor}" ]; then
+    		mkdir plotLoops_${res_arr[i]}_${coor}
 		else 
-    		echo -e "---  Using already created plotTADs_${res_arr[i]}_${coor}\n"		
+    		echo -e "---  Using already created plotLoops_${res_arr[i]}_${coor}\n"		
 		fi
 	  	
 	  		while IFS=":" read -r chr start_end; do
 				while IFS="-" read -r start end; do
-
-		       		hicPlotTADs --tracks ${arr[0]}/hicexplorer_results/${res_arr[i]}_resolution/tracks.ini -o ${arr[2]}/plotTADs_${res_arr[i]}_${coor}/${arr[0]}_${res_arr[i]}_TADs_${chr}_${start}-${end}.png --region ${coor} --dpi 300
-
+					
+					hicPlotMatrix -m ${arr[0]}/hicexplorer_results/${res_arr[i]}_resolution/inter_30_${res_arr[i]}_corrected.h5 -o ${arr[2]}/plotLoops_${res_arr[i]}_${coor}/${arr[0]}_${res_arr[i]}_loops_${chr}_${start}-${end}_matrix_loop.png --log1p --region ${coor} --loops ${arr[0]}/hicexplorer_results/${res_arr[i]}_resolution/loops.bedgraph --dpi 300
 
 				done <<< ${start_end}
 		  	done <<< ${coor}
